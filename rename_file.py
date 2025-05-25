7699346ab67657ef9077d89381dbac72e2412a04
= <=>
2025-05-25 18:09:24 +0700

    đ

--- rename_file.py
+++ rename_file.py
@@ -1,9 +1,13 @@
 import os
+from unidecode import unidecode
 
-def rename_files_replace_space_with_underscore(folder_path):
+def rename_files_replace_space_and_unicode(folder_path):
     for filename in os.listdir(folder_path):
-        if ' ' in filename:
+        if ' ' in filename or any(ord(c) > 127 for c in filename):
+            # Thay dấu cách bằng dấu _
             new_name = filename.replace(' ', '_')
+            # Chuyển unicode sang ASCII gần đúng
+            new_name = unidecode(new_name)
             old_path = os.path.join(folder_path, filename)
             new_path = os.path.join(folder_path, new_name)
             print(f'Renaming: "{filename}" -> "{new_name}"')
@@ -13,6 +17,6 @@ def rename_files_replace_space_with_underscore(folder_path):
 if __name__ == '__main__':
     folder = './lib/ipa/pdf'
     if os.path.isdir(folder):
-        rename_files_replace_space_with_underscore(folder)
+        rename_files_replace_space_and_unicode(folder)
     else:
         print('Đường dẫn không hợp lệ!')
